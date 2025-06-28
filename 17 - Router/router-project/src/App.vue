13678404f0d88fe1c1e84295076dc36f967e950b
devopMarkz <marcosdev2002@gmail.com>
2025-06-27 21:55:30 -0300

    Router

---
+++ 17 - Router/router-project/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/cursos">Cursos</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+}
+</style>
---
+++ 17 - Router/router-project/src/views/CursoView.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Essa é a Cursos</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
